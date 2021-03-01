da6d2621615c5c8924d2085c25e57258c43f5034
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2021-03-01 23:08:49 +0100

    Only first video is used, corrected alert

--- video/html5_playcontrol.js
+++ video/html5_playcontrol.js
@@ -7,7 +7,7 @@ if (window.dkr.videos == null){
     alert("No videos found; Play control not loaded");
 }
 else{
-    alert("Play control loaded:\r\n w: normal speed \r\n a: slower ; s: toggle play/pause; d: faster \r\n w: pause and 5 seconds backward ");
+    alert("Play control loaded:\r\n w: normal speed \r\n a: slower ; s: toggle play/pause; d: faster \r\n x: pause and 5 seconds backward ");
     document.addEventListener ('keydown', function (event) {
         if ((new URLSearchParams(document.location.search)).get("debug") == 1) {
             debugger;
@@ -15,22 +15,22 @@ else{
 
         //a: slower
         if (event.which == 65) {    
-            window.dkr.videos.forEach(v => v.playbackRate -= window.dkr.speedChange);
+            window.dkr.videos[0].playbackRate -= window.dkr.speedChange;
         }
 
         //d: faster
         if (event.which == 68) {
-            window.dkr.videos.forEach(v => v.playbackRate += window.dkr.speedChange);
+            window.dkr.videos[0].playbackRate += window.dkr.speedChange;
         }
 
         // s: toggle pause/play
         if (event.which == 83) {
             if (window.dkr.playing)      {
                 //Stops and five seconds backward
-                window.dkr.videos.forEach(v => {v.pause();});        
+                window.dkr.videos[0].pause();        
             }
             else{
-                window.dkr.videos.forEach(v => v.play());
+                window.dkr.videos[0].play()
             }
             window.dkr.playing = !window.dkr.playing;
         }
@@ -38,11 +38,12 @@ else{
         // x: stop and 5 seconds backwards
         if (event.which == 88) {
             window.dkr.playing = !window.dkr.playing;
-            window.dkr.videos.forEach(v => {v.pause(); v.currentTime -= 5});                    
+            window.dkr.videos[0].pause();
+            window.dkr.videos[0].currentTime -= 5;                    
         }
         // w: stop and 5 seconds backwards
         if (event.which == 87) {
-            window.dkr.videos.forEach(v => {v.playbackRate = 1});                    
+            window.dkr.videos[0].playbackRate = 1;                    
         }
     });
 }
